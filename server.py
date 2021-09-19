7e075678fcecc865fc876bfba580c7176b5295df
Dalton Ronan <dronan@ualberta.ca>
2021-09-19 12:31:36 -0600

    add server code

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,11 +29,68 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
+    def build_response_header(self, protocol, code, mime):
+        messages = {'200': 'OK', '301': 'Moved Permanently', '404': 'Not Found', '405': 'Method Not Allowed'}
+
+        head = ' '.join([protocol, code, messages[code]])+'\r\n'
+        content_type = 'Content-Type: text/{}; charset=utf-8\r\n'.format(mime)
+
+        return head + content_type + '\r\n'
+
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        # decode the incoming request so we can parse it
+        request = self.data.decode()
+
+        # Parse the headers 
+        headers = request.split('\n')[0].split()
+        method, filename, protocol = headers
+        
+        # this server can only handle GET; return 405 for any other method
+        if method != 'GET':
+            response = self.build_response_header(protocol, '405', 'html')
+            self.request.sendall(bytearray(response, 'utf-8'))
+            return
+        
+        # parse path and file, strip all ../ from path to be secure (i.e. keep it in root)
+        path, file = os.path.split(filename)
+        path = path.strip('../')
+
+        if file == '':
+            file += 'index.html'
+        try:
+            if path:
+                page = open('/'.join(['www', path, file]))
+            else:
+                page = open('/'.join(['www', file]))
+            content = page.read()
+            page.close()
+            mime = file.split('.')[-1]
+            response = self.build_response_header(protocol, '200', mime) + content
+            self.request.sendall(response.encode())
+        except Exception:
+            # file not found, try to redirct or send 404
+            try:
+                if path:
+                    page = open('/'.join(['www', path, file, 'index.html']))
+                else:
+                    page = open('/'.join(['www', file, 'index.html']))
+                
+                # if we can open page, send 301 and redirect
+                response = self.build_response_header(protocol, '301', 'html')
+                self.request.sendall(response.encode())
+                content = page.read()
+                page.close()
+                mime = file.split('.')[-1]
+                response = self.build_response_header(protocol, '200', mime) + content
+                self.request.sendall(response.encode())
+            except Exception:
+                response = self.build_response_header(protocol, '404', 'html')
+                self.request.sendall(response.encode())
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
